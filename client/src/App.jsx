0e9b82bf239a934212bfc26e854f01357bd6b074
Anubhav <aadhikari1248@gmail.com>
2023-10-01 17:00:22 +0530

    testing a new btn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,16 +10,20 @@ const App = () => {
         <Link to="/">
           <img src={logo} alt="logo" className='w-28 object-contain'/>
         </Link>
-        <Link to="/create-post" className="font-inter font-medium bg-[#7A316F] text-white px-4 py-2 rounded-md animate-bounce">
-            Create
+        <Link to="/create-post" className="relative inline-block px-4 py-2 font-medium group">
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+              <span className="relative text-black group-hover:text-white">Create</span>
+              
         </Link>
+        
       </header>
       <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />}/>
         </Routes> 
-      </main>
+      </main>                                     
     </BrowserRouter>
   )
 }
